97916647f193a23693432e955ba42ee7452d7a22
Swastik Haldar <swastik.haldar@gmail.com>
2017-01-13 16:56:49 +0530

    added user options

--- bss.c
+++ bss.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_route()
+{
+  FILE *route
+
 int main()
 {
   FILE *f;
   f= fopen("busdetail.txt","a");
+  printf("Welcome to IIT Kharagpur bus service system \n");
+  printf("HOME\n");
+  printf("1.Get bus routes between two stops\n");
+  printf("2.Get all the bus routes\n");
+  printf("3.Get all the bus routes through a specific bus stop\n");
+  printf("4.Get all bus stops associated with a specific route\n");
+  printf("5.Booking\n");
+  printf("6.Check availability\n");
+  printf("7.Pickup and drop facility between kharagpur railway station and IIT KGP");
+  printf("8.Register a complaint");
 
 }
