004696b79921c16265829612e853e2edf8a47aa6
Swastik Haldar <swastik.haldar@gmail.com>
2017-01-16 21:52:02 +0530

    code now correctly displays all routes with a start point and destination point

--- bss.c
+++ bss.c
@@ -30,8 +30,8 @@ _stops stops;
 void initialize()
 {
   stops.no_stop=0;
-
 }
+
 void readstops()
 {
   char *st_name;
@@ -71,13 +71,13 @@ void readroute()
       while((read = getline(&line, &len, fptr)) != -1)
 	{
 	  printf("%s",line);
-	  stname=strtok(line," ,.-");
+	  stname=strtok(line," ,.-\n");
 	  stop_no=0;
 	  while(stname!=NULL)
 	    {
 	      strcpy(routes.no[route_no].route[stop_no].name,stname);
 	      printf("%s\n",routes.no[route_no].route[stop_no].name);
-	      stname=strtok(NULL," ,.-");
+	      stname=strtok(NULL," ,.-\n");
 	      stop_no++;
 	    }
 	  routes.no[route_no].num_stop=stop_no;
@@ -110,6 +110,14 @@ void print_all_routes()
 	}
     }
 }
+void get_bus_stop(char *s)
+{
+  printf("choose your stop:\n");
+  print_all_stops();
+  int a;
+  scanf("%d",&a);
+  strcpy(s,stops.nam[a-1].name);
+}
 
 void routes_between_stops()
 {
@@ -117,9 +125,9 @@ void routes_between_stops()
   char start[100],des[100];
   printf("Enter Starting and Destination Bus stop:\n");
   printf("Starting Bus Stop:");
-  scanf("%s",start);
+  get_bus_stop(start);
   printf("Destination Bus Stop:");
-  scanf("%s",des);
+  get_bus_stop(des);
   printf("Route No. -- Bus Start from -- Bus Goes to\n");
   for(i=0;i<routes.no_route;i++)
     {
@@ -128,11 +136,16 @@ void routes_between_stops()
 	{
 	  if(strcmp(routes.no[i].route[j].name,start)==0)
 	    {
+	      //printf("%s == %s\n",routes.no[i].route[j].name,start);
+	      //printf("%s , %s\n",routes.no[i].route[routes.no[i].num_stop-1].name,des);
+	      //printf("%zu -- %zu\n",strlen(routes.no[i].route[routes.no[i].num_stop-1].name),strlen(des));
 	      for(k=j+1;k<routes.no[i].num_stop;k++)
 		{
+		  //printf("%s !== %s",routes.no[i].route[k].name,des);
 		  if(strcmp(routes.no[i].route[k].name,des)==0)
 		    {
 		      f=1;
+ 
 		      break;
 		    }
 		}
@@ -142,6 +155,7 @@ void routes_between_stops()
 	}
       if(f==1)
 	{
+  
 	  printf("%d     -----     %s   -----   %s \n",i+1,routes.no[i].route[0].name,routes.no[i].route[routes.no[i].num_stop-1].name);
 	}
     }
